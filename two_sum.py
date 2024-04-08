27b8edf1f8e5563132eef0db3cb9428c7335dcbc
Sebastián Diaz <99108952+SebasMas@users.noreply.github.com>
2024-04-08 14:38:51 -0500

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        prevMap = {} # Para guardar el valor y su índice
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+
+        return []
+
+tar = 6
+n = [3,3]
+s = Solution()
+print(s.twoSum(n, tar))# two_sum
